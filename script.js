df8bceea587690e031acdad5196f3c3c3809317e
m5teh <51937988+m5teh@users.noreply.github.com>
2020-11-17 23:23:39 +0700

    thay doi

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-function changeColor2 () {
-    document.getElementById("tab-2").style.backgroundColor = "brown";
-    document.getElementById("tab-1").style.backgroundColor = "red";
-    document.getElementById("tab-3").style.backgroundColor = "red";
-    document.getElementsByClassName("infomation")[0].style.display = "none";
-    document.getElementsByClassName("education")[0].style.display = "inline-block";
-    document.getElementsByClassName("hobby")[0].style.display = "none";
+function showAdd () {
+    $(".add-button").show(300);
 }
-function changeColor3() {
-    document.getElementById("tab-3").style.backgroundColor = "brown";
-    document.getElementById("tab-1").style.backgroundColor = "red";
-    document.getElementById("tab-2").style.backgroundColor = "red";
-    document.getElementsByClassName("infomation")[0].style.display = "none";
-    document.getElementsByClassName("education")[0].style.display = "none";
-    document.getElementsByClassName("hobby")[0].style.display = "inline-block";
-}
-function changeColor () {
-    document.getElementById("tab-1").style.backgroundColor = "brown";
-    document.getElementById("tab-2").style.backgroundColor = "red";
-    document.getElementById("tab-3").style.backgroundColor = "red";
-    document.getElementsByClassName("infomation")[0].style.display = "inline-block";
-    document.getElementsByClassName("education")[0].style.display = "none";
-    document.getElementsByClassName("hobby")[0].style.display = "none";
-}
+$(".input-text").focus (
+    function () {
+        $(".input-text").animate ({
+            width : '80%'
+        }, 500, showAdd)
+        
+    }
+)
+$(".add-button").click (
+    function () {
+        var addContent = $(".input-text").val();
+        if (! addContent.trim()) {
+            alert('cc');
+            return;
+        }
+         var task = document.createElement("div");
+         var checkk = document.createElement("i");
+         var deletee = document.createElement("i");
+         var cont = document.createElement("p");
+        $(checkk).addClass("fas fa-circle check");
+         $(deletee).addClass ("fas fa-trash delete");
+        cont.innerHTML = addContent;
+        $(task).addClass("task");
+        $(task).append(checkk, deletee,cont);
+        $(".list-task").append(task);
+        $(".add-button").hide();
+        $(".input-text").animate ({
+            width : '100px'
+        }, 500)
+        $(".input-text").val("");
+    }
+) 
+$(document).on ("click",".delete", function() {
+    $(this).parent().hide();
+})
+$(document).on ("click",".check", function() {
+    $(this).css("color", "#f6b93b");
+    $(this).parent().children("p").css ({"font-style":"italic","font-size":"120%"});
+})
